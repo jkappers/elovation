ef88e7adaf5339043ebd33d2d2d870d2a9efecf0
Joshua Kappers <josh@jkappers.me>
2014-01-16 16:17:27 -0600

    Include devise spec helpers. Set password in player factory

--- spec/factories/player_factory.rb
+++ spec/factories/player_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :player do
-    name { Faker::Name.name }
-    email { Faker::Internet.email }
+    name 		 { Faker::Name.name }
+    email 	 { Faker::Internet.email }
+    password "12345678"
   end
 end
